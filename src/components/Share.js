3140b0f7e1302e84fc0b5fbcc8c80de777e4654b
unknown <16054cn021@gmail.com>
2022-07-06 23:27:43 +0530

    fetch updated

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,52 +1,57 @@
-import React,{useState} from 'react'
-import { auth ,db} from '../firebase.js';
-import './share.css'
-import Fetch from "../components/fetch"
-import firebase from"firebase";
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { auth, db } from "../firebase.js"
+import "./share.css"
+
 export default function Share() {
-    const[popup,setPopup]=useState(false);
+  const [popup, setPopup] = useState(false)
+  const navigate = useNavigate()
 
-    const baseUrL = 'http://localhost:3000/homepage';
+  const baseUrL = "http://localhost:3000/homepage"
 
-    const  sharedURL = `${baseUrL}/Share?${auth.currentUser.uid}`;
-    function getUserData(uid) {
-            firebase.database().ref('users/' + uid).once("value", snap => {
-            console.log(snap.val())
-        })
-    }
-    const openPopup = () => {
-        setPopup(true);
-    }
+  const sharedURL = `${baseUrL}/Share?${auth.currentUser.uid}`
 
-    const closePopup=()=>{
-        setPopup(false);
-    }
-    
+  function getUserData(uid) {
+    db.ref("users/" + uid).once("value", (snap) => {
+      console.log(snap.val())
+    })
+  }
+  const openPopup = () => {
+    setPopup(true)
+  }
+
+  const closePopup = () => {
+    setPopup(false)
+  }
 
   return (
-   
-    <div>Share
-        <h1>Todo-List</h1>
-        <button className="url" onClick={openPopup} > create url</button>
-        <div>
-            {popup ?
-            <div>
-                <div className="popup">
-                    <div className='popup-header'>
-                   <h1> here is your url</h1>
-                   <button className= "btn" onClick={closePopup}>x</button> 
-                   
-                   </div>
-                    <div>
-                    <p>{sharedURL}</p>
-                    </div>
-                </div>
-            </div>:""}
-        </div>
+    <div>
+      Share
+      <h1>Todo-List</h1>
+      <button className="url" onClick={openPopup}>
+        {" "}
+        create url
+      </button>
+      <button onClick={() => navigate("/fetch")}>Fetch</button>
+      <div>
+        {popup ? (
+          <div>
+            <div className="popup">
+              <div className="popup-header">
+                <h1> here is your url</h1>
+                <button className="btn" onClick={closePopup}>
+                  x
+                </button>
+              </div>
+              <div>
+                <p>{sharedURL}</p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
-
-
-
-    
   )
 }
--- src/components/fetch.js
+++ src/components/fetch.js
@@ -1,14 +1,35 @@
-import React from "react";
-import firebase from"firebase";
+import { useState, useEffect } from "react"
+import { auth, db } from "../firebase.js"
+import { uid } from "uid"
+import { collection, onSnapshot, query, where } from "firebase/firestore"
 
+export default function Fetch() {
+  const [todos, setTodos] = useState()
+  const getTodos = () => {
+    onSnapshot(
+      query(
+        collection(db, "tasks"),
+        where("owner", "==", auth.currentUser.uid)
+      ),
+      (snapshots) => {
+        setTodos([])
+        const List = []
+        snapshots.forEach((doc) => {
+          List.push(doc.data())
+        })
+        setTodos(List)
+      }
+    )
+  }
+  useEffect(() => {
+    getTodos()
+  })
 
-function Fetch(){
-
-
-return{
-   
-    
+  return (
+    <>
+      {todos.map((todo) => {
+        return <h1>{todo.title}</h1>
+      })}
+    </>
+  )
 }
-
-    }
-export default Fetch;
